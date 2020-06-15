2f938dda6ed64d39e568a293d0a499e051b1cb56
Sam Lee <samuel.r.lee@vanderbilt.edu>
2020-06-15 11:36:50 -0500

    Adding the relevant files saved from previous git disaster

--- python_scripts/baseline.py
+++ python_scripts/baseline.py
@@ -2,7 +2,8 @@ import os, sys
 import correlations
 import numpy as np
 from PIL import Image
-from scipy.stats import spearmanr
+import matplotlib.pyplot as plt
+from scipy.stats import spearmanr, pearsonr
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
@@ -26,7 +27,7 @@ def transform_baseline(imgset, transform, full_model, layer_num, correlate_func)
         - shift: pixel-by-pixel translation, fill with gray, each "depth" is mean of up-down-left-right shifting
     '''
 
-    print('Transform:', transform, 'with', correlate_func.__name__)
+    print('### Transform:', transform, 'with', correlate_func.__name__)
     num_imgs = imgset.shape[0]
     dim = imgset.shape[1]
     correlations = []
@@ -35,32 +36,33 @@ def transform_baseline(imgset, transform, full_model, layer_num, correlate_func)
     inp = full_model.input
     layer = full_model.layers[layer_num]
     out = layer.output
-
     # Flatten if necessary and using RSA
     if len(out.shape) != 2 and correlate_func == do_rsa:
             out = Flatten()(out) 
+    
+    model = Model(inputs=inp, outputs=out)
 
     # Get reps for originals
-    rep_orig = model.predict(imageset)
+    rep_orig = model.predict(imgset)
     # Set orig RDM if using RSA
     if correlate_func == do_rsa:
         rep_orig = get_rdm(rep_orig)
 
     if transform == 'reflect':
-        print('Working on version 1 of 1')
-        transformed_imgset = np.reflect(imgset)
-        rep = model.predict(transformed_imgset, verbose=1)
-        print('Now correlating...')
+        print(' - Working on version 1 of 1')
+        transformed_imgset = np.flip(imgset, axis=2)
+        rep = model.predict(transformed_imgset, verbose=0)
+        print(' - Now correlating...')
         correlations.append(correlate_func(rep_orig, rep))
 
-    elif transform == 'color':Flatten
-        versions = 200
+    elif transform == 'color':
+        versions = 201
         transformed_imgset = np.empty((num_imgs, dim, dim, 3), dtype=np.uint8)
         alphas = np.linspace(-0.1, 0.1, versions)
 
         for v in range(versions):
             # Generate transformed imageset
-            print('Working on version', v, 'of', versions)
+            print(' - Working on version', v, 'of', versions)
             alpha = alphas[v]
             for i in range(num_imgs):
                 img = imgset[i, :, :, :]
@@ -71,59 +73,66 @@ def transform_baseline(imgset, transform, full_model, layer_num, correlate_func)
                 change = np.dot(vectors, (values * [alpha, alpha, alpha]).T)
                 transformed_imgset[i, :, :, :] = np.clip(img + change, a_min=0, a_max=255, out=None)
             
-            print('Now correlating...')
-            rep = model.predict(transformed_imgset, verbose=1)
+            print(' - Now correlating...')
+            rep = model.predict(transformed_imgset, verbose=0)
             correlations.append(correlate_func(rep_orig, rep))
+            print('correlation:', correlations[v])
             
     elif transform == 'zoom':
-        versions = dim / 2
+        versions = dim // 2
         transformed_imgset = np.zeros((num_imgs, dim, dim, 3), dtype=np.uint8)
         for v in range(versions):
             # Generate transformed imageset
-            print('Working on version', v, 'of', versions)
+            print(' - Working on version', v, 'of', versions)
             for i in range(num_imgs):
                 img = Image.fromarray(imgset[i, :, :, :])
                 new_img = img.crop((v, v, dim - v, dim - v))
-                new_img = newImg.resize((dim, dim), resample=Image.BICUBIC)
+                new_img = new_img.resize((dim, dim), resample=Image.BICUBIC)
                 new_img = img_to_array(new_img).astype(np.uint8)
                 transformed_imgset[i, :, :, :] = new_img
                 
-            print('Now correlating...')
-            rep = model.predict(transformed_imgset, verbose=1)
+            print(' - Now correlating...')
+            rep = model.predict(transformed_imgset, verbose=0)
             correlations.append(correlate_func(rep_orig, rep))
+            print('correlation:', correlations[v])
 
     elif transform == 'shift':
         versions = dim
-        up_imgset = np.zeros((img_count, dim, dim, 3), dtype=np.uint8)
-        down_imgset = np.zeros((img_count, dim, dim, 3), dtype=np.uint8)
-        left_imgset = np.zeros((img_count, dim, dim, 3), dtype=np.uint8)
-        right_imgset = np.zeros((img_count, dim, dim, 3), dtype=np.uint8)
+        up_imgset = np.zeros((num_imgs, dim, dim, 3), dtype=np.uint8)
+        down_imgset = np.zeros((num_imgs, dim, dim, 3), dtype=np.uint8)
+        left_imgset = np.zeros((num_imgs, dim, dim, 3), dtype=np.uint8)
+        right_imgset = np.zeros((num_imgs, dim, dim, 3), dtype=np.uint8)
         # create the gray values we use to fill in space
         empty = np.zeros((dim, dim, 3)).astype(np.uint8)
         empty.fill(128)
         for v in range(versions):
             # Generate transformed imageset
-            print('Working on version', v, 'of', versions)
-            for i in range(img_count):
-                img = imgmset[i, :, :, :, 0]
-                up_imgset[v]    = np.concatenate([img[v:pixels, :, :], empty[0:v, :, :]])
-                down_imgset[v]  = np.concatenate([empty[0:v, :, :], img[0:pixels - v, :, :]])
-                left_imgset[v]  = np.concatenate([img[:, v:pixels, :], empty[:, 0:v, :]], axis=1)
-                right_imgset[v] = np.concatenate([empty[:, 0:v, :], img[:, 0:pixels - v, :]], axis=1)
+            print(' - Working on version', v, 'of', versions)
+            for i in range(num_imgs):
+                img = imgset[i, :, :, :]
+                up_imgset[i]    = np.concatenate([img[v:dim, :, :], empty[0:v, :, :]])
+                down_imgset[i]  = np.concatenate([empty[0:v, :, :], img[0:dim - v, :, :]])
+                left_imgset[i]  = np.concatenate([img[:, v:dim, :], empty[:, 0:v, :]], axis=1)
+                right_imgset[i] = np.concatenate([empty[:, 0:v, :], img[:, 0:dim - v, :]], axis=1)
 
             # Get average of all 4 directions
-            print('Now correlating...')
+            print(' - Now correlating...')
             corr_sum = 0.
-            rep = model.predict(up_imgset, verbose=1)
+            rep = model.predict(up_imgset, verbose=0)
             corr_sum += correlate_func(rep_orig, rep)
-            rep = model.predict(down_imgset, verbose=1)
+            print('corr_sum:', corr_sum)
+            rep = model.predict(down_imgset, verbose=0)
             corr_sum += correlate_func(rep_orig, rep)
-            rep = model.predict(left_imgset, verbose=1)
+            print('corr_sum:', corr_sum)
+            rep = model.predict(left_imgset, verbose=0)
             corr_sum += correlate_func(rep_orig, rep)
-            rep = model.predict(right_imgset, verbose=1)
+            print('corr_sum:', corr_sum)
+            rep = model.predict(right_imgset, verbose=0)
             corr_sum += correlate_func(rep_orig, rep)
+            print('corr_sum:', corr_sum)
             correlations.append(corr_sum / 4)
 
+    print('Done!\n')
     return correlations
 
 '''
@@ -185,9 +194,55 @@ def find_threshold(acts):
     return ans
 
 def get_rdm(rep):
+    print('shape:', rep.shape)
     num_imgs = rep.shape[0]
     print('num_images =', num_imgs)
     return spearmanr(rep.T, rep.T)[0][0:num_imgs, 0:num_imgs]
+
+"""
+Sanity check/Visualization functions
+"""
+def visualize_transform(transform, depth, img_arr):
+    if transform == 'reflect':
+        # Depth doesn't matter
+        transformed = np.flip(img_arr, axis=1)
+        plt.imshow(transformed)
+        
+    elif transform == 'color':
+        # Depth = alpha value * 1000 (ints -100 : 100)
+        alpha = depth / 1000
+        img_reshaped = img_arr.reshape(-1, 3)
+        cov = np.cov(img_reshaped.T)
+        values, vectors = np.linalg.eig(cov)
+        change = np.dot(vectors, (values * [alpha, alpha, alpha]).T)
+        new_img = np.round(img_arr + change)
+        transformed = np.clip(new_img, a_min=0, a_max=255, out=None).astype('uint8')
+        plt.imshow(transformed)
+            
+    elif transform == 'zoom':
+        dim = img_arr.shape[0]
+        v = depth
+        img = Image.fromarray(img_arr)
+        new_img = img.crop((v, v, dim - v, dim - v))
+        new_img = new_img.resize((dim, dim), resample=Image.BICUBIC)
+        transformed = img_to_array(new_img).astype(np.uint8)
+        plt.imshow(transformed)
+         
+    elif transform == 'shift':
+        dim = img_arr.shape[0]
+        v = depth
+        empty = np.zeros((dim, dim, 3)).astype(np.uint8)
+        up_transformed    = np.concatenate([img_arr[v:dim, :, :], empty[0:v, :, :]])
+        down_transformed  = np.concatenate([empty[0:v, :, :], img_arr[0:dim - v, :, :]])
+        left_transformed  = np.concatenate([img_arr[:, v:dim, :], empty[:, 0:v, :]], axis=1)
+        right_transformed = np.concatenate([empty[:, 0:v, :], img_arr[:, 0:dim - v, :]], axis=1)
+        
+        f, axarr = plt.subplots(2,2)
+        axarr[0,0].imshow(up_transformed)
+        axarr[0,1].imshow(down_transformed)
+        axarr[1,0].imshow(left_transformed)
+        axarr[1,1].imshow(right_transformed)
+        
 
 
 
--- python_scripts/correlations.py
+++ python_scripts/correlations.py
@@ -4,16 +4,20 @@ from tensorflow.keras.models import load_model, Model
 from tensorflow.keras.layers import Flatten
 from scipy.stats import pearsonr, spearmanr
 
-def make_test_data():
+def make_test_data(whiten=True):
     '''
     Obtain data as described in paper, 1000 images, 100 per category
-    Post: Returns curated dataset
+    Post: Returns curated dataset, also whitened if necessary
     '''
     print('Making test data...')
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
+    if whiten:
+        x_test = np.load('../data/cifar10_modified/x_test_new.npy')
+        y_test = np.load('../data/cifar10_modified/y_test_new.npy')
+    else:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        
     x_predict = np.empty((1000, 32, 32, 3))
-    y_predict = np.empty((1000, 1))
+    y_predict = np.empty((1000, 10)) if whiten else np.empty((1000, 1))
 
     # Fill up 10 different categories with 100 images
     full = False
@@ -22,18 +26,19 @@ def make_test_data():
     i = 0
     while not full:
         x = x_test[i]
-        y = y_test[i] # also serves as index
+        y = y_test[i]
         # Check if the category is full first, otherwise skip
-        cur_count = counts[y[0]]
+        index = np.argmax(y) if whiten else y[0] # y[0] because non-categorical y is an array of len 1
+        cur_count = counts[index]
         if cur_count != 100:
-            x_predict[100 * y[0] + cur_count] = x
-            y_predict[100 * y[0] + cur_count] = y
-            counts[y[0]] += 1
+            x_predict[100 * index + cur_count] = x
+            y_predict[100 * index + cur_count] = y
+            counts[index] += 1
         # Quit when all categories are full
         full = all(count == 100 for count in counts)
         i += 1
     print('Done!')
-    return x_predict, y_predict
+    return x_predict.astype('uint8'), y_predict
 
         
 def make_RDMs(model, layer_arr, x_predict):
