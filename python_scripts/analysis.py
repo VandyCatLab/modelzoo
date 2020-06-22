040be896f74e2f357e5d8a8c93553e3f9da11d39
Sam Lee <samuel.r.lee@vanderbilt.edu>
2020-06-22 18:40:06 -0400

    Consolidated high level functions into a single one, still need plenty of testing

--- python_scripts/analysis.py
+++ python_scripts/analysis.py
@@ -11,55 +11,13 @@ import sys
 sys.path.append('../imported_code/svcca')
 import cca_core, pwcca
 
-'''
-High level, high specificity functions
-'''
-def RSA_specific(path_to_instances, identifiers, x_predict):
+def correlate(correlation_function, path_to_instances, identifiers, x_predict):
     '''
     Pre: ***HARDCODED*** 10 instances at specified path with 9 layers each, 1000 images
-    Post: returns 90x90 correlation matrix using RSA
+    Post: returns 90x90 correlation matrix using RSA, SVCCA or PWCCA
     '''
-    # Create container for RDMs
-    RDM_list = np.empty((90, 1000, 1000)) # shape = #layers * #instances, #images, #images
-
-    # Loop through network instances to get RDMs
-    print('**** Gather RDMs ****')
-    instance_num = 0
-    for i in identifiers:
-        print('*** Working on model', str(i), '***')
-        K.clear_session()
-        model = load_model(path_to_instances + str(i) + '.h5')
-        print('** First, get acts')
-        acts_list = get_acts(model, range(9), x_predict)
-        print('** Second, get RDMs')
-        # Loop through layers
-        layer_num = 0
-        for acts in acts_list:
-            # Flatten if necessary
-            # TODO: Haven't tested this method of reshaping yet, look here first if things break
-            if len(acts.shape) > 2:
-                imgs, h, w, channels = acts1.shape
-                acts = np.reshape(acts, newshape=(imgs, h*w*channels))
-            RDM_list[10 * layer_num + instance_num] = get_rdm(acts.T)
-            layer_num += 1
-        instance_num += 1
-
-    # Create correlation matrix with 2nd-level RSA
-    print('**** Done gathering RDMs, now correlations ****')
-    correlations = np.empty((90, 90))
-    for i in range(correlations.shape[0]):
-        for j in range(correlations.shape[1]):
-            print('Correlation', str(i), ',', str(j))
-            correlations[i, j] = do_rsa_second_level_only(RDM_list[i], RDM_list[j])
-    
-    print('Done!')
-    return correlations
-
-def SVCCA_specific(path_to_instances, identifiers, x_predict):
-    '''
-    Pre: ***HARDCODED*** 10 instances at specified path with 9 layers each, 1000 images
-    Post: returns 90x90 correlation matrix using SVCCA
-    '''
+    # Check ahead of time for typos
+    assert correlation_function in [do_rsa, do_svcca, do_pwcca]
     print('**** Load and Preprocess Acts ****')
     # Load up all acts into layer * instance grid
     all_acts = [[], [], [], [], [], [], [], [], []]
@@ -74,18 +32,24 @@ def SVCCA_specific(path_to_instances, identifiers, x_predict):
         layer_num = 0
         for acts in acts_list:
             # Flatten by averaging if conv layer
-            acts = np.mean(acts, axis=(1,2)) if acts.shape > 2 else acts
+            if len(acts.shape) > 2:
+                if correlation_function == do_rsa:
+                    imgs, h, w, channels = acts.shape
+                    acts = np.reshape(acts, newshape=(imgs, h*w*channels))
+                else:
+                    acts = np.mean(acts, axis=(1,2))             
             all_acts[layer_num].append(acts.T)
             layer_num += 1
     
+    # Now do correlations
     print('**** Done gathering RDMs, now correlations ****')
     num_networks = 10
-    correlations = np.empty((90, 90))
+    correlations = np.zeros((90, 90))
     # Run SVCCA
     for i in range(correlations.shape[0]):
-        for j in range(correlations.shape[1]):
+        for j in range(correlations.shape[1] - i):
             print('Correlation', str(i), ',', str(j))
-            
+            # Decode into the org scheme we want (i.e. layer * instance)
             layer_i = i // num_networks
             network_i = i % num_networks
             layer_j = j // num_networks
@@ -93,36 +57,30 @@ def SVCCA_specific(path_to_instances, identifiers, x_predict):
             acts1 = all_acts[layer_i][network_i]
             acts2 = all_acts[layer_j][network_j]
             
-            # Do svcca
-            correlations[i, j] = do_svcca(acts1, acts2)
+            correlations[i, j] = correlation_function(acts1, acts2)
     
+    # OK this part might be a bit sketch. Basically, grab all the non-diag values
+    # into a second matrix, then transpose to flip, then add it to the orig matrix.
+    # This makes it so we don't need to compute every value twice
+    num_imgs = 90
+    correlations_lower = correlations[np.triu_indices(n=num_imgs, k=1)].T
+    correlations += correlations_lower
+
     print('Done!')
     return correlations
-
-def PWCCA_specific(path_to_instances, identifiers, x_predict):
-    '''
-    Pre: ***HARDCODED*** 10 instances at specified path with 9 layers each, 1000 images
-    Post: returns 90x90 correlation matrix using SVCCA
-    '''
-
 
 '''
 Correlation analysis functions
 '''
 def do_rsa(acts1, acts2):
-    '''
-    Wrapper for do_rsa_second_level_only (for easy passing to baseline)
-    '''
     rdm1 = get_rdm(acts1)
     rdm2 = get_rdm(acts2)
-    return do_rsa_second_level_only(rdm1, rdm2)
-    
-def do_rsa_second_level_only(rdm1, rdm2):
     assert rdm1.shape == rdm2.shape
     num_imgs = rdm1.shape[0]
+    # Only use upper-triangular values
     rdm1_flat = rdm1[np.triu_indices(n=num_imgs, k=1)]
     rdm2_flat = rdm2[np.triu_indices(n=num_imgs, k=1)]
-    return pearsonr(rdm1_flat, rdm2_flat)[0]
+    return pearsonr(rdm1_flat, rdm2_flat)[0]    
 
 def do_svcca(acts1, acts2):
     # Transpose to get shape [neurons, datapoints]
@@ -132,8 +90,8 @@ def do_svcca(acts1, acts2):
     cacts1 = acts1.T - np.mean(acts1.T, axis=1, keepdims=True)
     cacts2 = acts2.T - np.mean(acts2.T, axis=1, keepdims=True)
     # Perform SVD
-    U1, s1, V1 = np.linalg.svd(cacts1, full_matrices=False)
-    U2, s2, V2 = np.linalg.svd(cacts2, full_matrices=False)
+    _, s1, V1 = np.linalg.svd(cacts1, full_matrices=False)
+    _, s2, V2 = np.linalg.svd(cacts2, full_matrices=False)
 
     svacts1 = np.dot(s1[:threshold1]*np.eye(threshold1), V1[:threshold1])
     svacts2 = np.dot(s2[:threshold2]*np.eye(threshold2), V2[:threshold2])
@@ -194,7 +152,3 @@ def get_rdm(acts):
     num_imgs = rep.shape[0]
     print('num_images =', num_imgs)
     return spearmanr(rep.T, rep.T)[0][0:num_imgs, 0:num_imgs]
-
-
-
-    
