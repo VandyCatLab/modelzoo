28465c784e955d5709a1b709ca8e31069a69eba1
JasonKChow <jason.k.chow@vanderbilt.edu>
2021-10-19 00:35:50 -0500

    create big ratio sims

--- python_scripts/simulation.py
+++ python_scripts/simulation.py
@@ -147,6 +147,55 @@ def sizeRatioTest():
     permuteSims.to_csv("../outputs/masterOutput/ratioSims.csv", index=False)
 
 
+def bigSizeRatioTest():
+    # load dataset
+    imgset = np.load("../outputs/masterOutput/bigDataset.npy")
+    model = tf.keras.applications.MobileNetV3Small(input_shape=(224, 224, 3))
+    model.compile(metrics=["top_k_categorical_accuracy"])
+    print(f"Model loaded: MobileNetV3Small", flush=True)
+    rep = model.predict(imgset).flatten()
+
+    preprocFuns = [
+        analysis.preprocess_rsaNumba,
+        analysis.preprocess_svcca,
+        analysis.preprocess_ckaNumba,
+    ]
+    simFuns = [
+        analysis.do_rsaNumba,
+        analysis.do_svcca,
+        analysis.do_linearCKANumba,
+    ]
+    colNames = [fun.__name__ for fun in simFuns] + ["sample", "features"]
+
+    permuteSims = pd.DataFrame(columns=colNames)
+
+    nMax = imgset.shape[0]
+    ratiosRange = np.arange(0.05, 1, 0.05)
+    nPermute = 10000
+
+    for ratio in ratiosRange:
+        print(f"Analyzing img:samples ratio: {ratio}")
+        repShape = (nMax, int(nMax * ratio))
+        for permute in range(nPermute):
+            if permute % 100 == 0:
+                print(f"Permutation at {permute}")
+
+            # Sample reps
+            rep1 = np.random.choice(rep, size=repShape)
+            rep2 = np.random.choice(rep, size=repShape)
+
+            sims = analysis.multi_analysis(rep1, rep2, preprocFuns, simFuns)
+            sims["sample"] = repShape[0]
+            sims["features"] = repShape[1]
+            permuteSims = pd.concat(
+                (
+                    permuteSims,
+                    pd.DataFrame.from_dict(sims, orient="index").transpose(),
+                )
+            )
+    permuteSims.to_csv("../outputs/masterOutput/bigRatioSims.csv", index=False)
+
+
 def parametricAblation(minNeuron=3, maxNeuron=10):
     modelPath = "../outputs/masterOutput/models/w0s0.pb"
     print("Loading model")
